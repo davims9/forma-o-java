3457823616a670bb7b5a36e3fdf8661d3aae7459
davims9 <davimourasouza@gmail.com>
2022-03-30 22:50:10 -0300

    Fim da modulo 2

--- ByteBank-encapsulado/src/Conta.java
+++ ByteBank-encapsulado/src/Conta.java
@@ -1,14 +1,25 @@
 public class Conta {
-	private int numero;
+	private int numero; // atributos
 	private int agencia;
 	private Cliente titular;
 	private double saldo;
 	
-	public void deposita(double valor){
+	private static int total = 0; //static torna o atributo pertencenta classe 
+	
+	public Conta(int agencia, int numero){ //Isso é um construtor, que é diferente de um metodo
+	    this.agencia = agencia;
+	    this.numero = numero;
+	    this.saldo = 0;
+	    System.out.println("Estou criando uma conta");
+	    
+	    Conta.total++; //poderia ficar com o Conta. e mesmo assim compila 
+	}
+	
+	public void deposita(double valor){ // metodo
 		this.saldo += valor;
 	}
 	
-	public boolean saca(double valor) {
+	public boolean saca(double valor) { // metodo
 		if(this.saldo>=valor) {
 			this.saldo -= valor;
 			return true;
@@ -17,7 +28,7 @@ public class Conta {
 		}		
 	}
 	
-	public boolean transfere(double valor, Conta destino) {
+	public boolean transfere(double valor, Conta destino) { // metodo
 		if(this.saldo >= valor) {
 			this.saldo -= valor;
 			destino.deposita(valor);
@@ -27,11 +38,37 @@ public class Conta {
 		}
 	}
 	
-	public double getSaldo(){
+	public double getSaldo(){ // metodo
 		return this.saldo;
 	}
 	
+	/* retirado pois o numero já é setada no construtor
 	public void setNumero(int numero) {
 		this.numero = numero;
+	}
+	*/
+	public int getNumeto() {
+		return this.numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	/* retirado pois a agencia já é setada no construtor
+	public void setAgencia (int agencia) {
+		this.agencia = agencia;
+	}
+	*/
+	public Cliente getTitular() {
+		return this.titular;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public static int getTotal(){
+		return Conta.total;
 	}
 }
--- ByteBank-encapsulado/src/TestaMetodos.java
+++ ByteBank-encapsulado/src/TestaMetodos.java
@@ -1,4 +1,29 @@
-
 public class TestaMetodos {
-
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta(1919,999999);
+		
+		Cliente davi = new Cliente();
+		
+		conta.deposita(200);
+		
+		
+		davi.setNome("Davi Moura Souza"); //define nome = a Davi Moura Souza, mas o objeto davi 
+											//ainda não tinha vinculo com objeto conta
+		
+		conta.setTitular(davi);//referenciei a o obejto conta ao objeto davi
+		
+		conta.getTitular().setProfissao("Dev");
+		
+		System.out.println("Titular é  " + conta.getTitular());
+		System.out.println("Titular é  " + davi);
+		System.out.println("Titular é  " + conta.getTitular().getNome());
+		System.out.println("Titular é  " + conta.getTitular().getProfissao());
+		System.out.println("O Saldo da conta é de " + conta.getSaldo());
+		System.out.println(Conta.getTotal());
+		
+		Conta conta2 = new Conta(1999,9191919);
+		
+		System.out.println(Conta.getTotal());
+	}
 }
